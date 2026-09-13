b1ceb95c4647ea200db879636483527c44427a7f
verma0909k <coding0909k@gmail.com>
2026-09-13 22:22:19 +0530

    added difficulty levels  and attempts in guessing game

--- 01-Guessing-Number-Game/guessing_game.c
+++ 01-Guessing-Number-Game/guessing_game.c
@@ -2,27 +2,75 @@
 #include <time.h>
 #include <stdlib.h>
 
+void print_menu();
+
 int main() {
-   printf("\n-----------------------------\n");
-   printf("GUESSING NUMBER GAME\n");
-   printf("-----------------------------\n");
-   printf("\nWELCOME TO WORLD OF GUESSING NUMBERS.\n");
+   print_menu();
+
+    int choice,range;
+   scanf("%d",&choice);
+   int guess,max_attempts,attempts=0;
    srand(time(NULL));
-   int secret=rand()%100+1;
-   int guess;
-   do {
-      printf("\nGuess a number between(1 to 100) :");
+   
+   switch (choice)
+   {
+   case 1:
+      range=50;
+      max_attempts=10;
+      break;
+   case 2:
+      range=100;
+      max_attempts=7;
+      break;
+   case 3:
+      range=200;
+      max_attempts=6;
+      break;
+   case 4:
+      range=500;
+      max_attempts=5;
+      break;
+   default:
+      range=100;
+      max_attempts=7;
+      break;
+   }
+
+   int number=rand()%range+1;
+
+   while (attempts<max_attempts) {
+      printf("\nGuess a number between(1 to %d) :",range);
       scanf("%d",&guess);
-      if (secret>guess) {
+      attempts++;
+      if (number>guess) {
          printf("TOO LOW !\n");
-      } else if (secret<guess) {
+      } else if (number<guess) {
          printf("TOO HIGH !\n");
-      } else if (secret==guess) {
-         printf("CORRECT !, The number is %d\n",secret);
+      } else if (number==guess) {
+         printf("YOU WON ! The number was %d. you took %d attempts to guess it.\n",number,attempts);
+         break;
       }
-   } while (guess!=secret);
+      if (attempts==max_attempts) {
+         printf("\nGAME OVER! ,The number was %d\n",number);
+      }
+      else {
+         printf("ATTEMPTS LEFT :%d\n",max_attempts-attempts);
+      }
+   }
 
 
 
    return 0;
+}
+void print_menu() {
+   printf("\n-----------------------------\n");
+   printf("GUESSING NUMBER GAME\n");
+   printf("-----------------------------\n");
+   printf("\nWELCOME TO THE WORLD OF GUESSING NUMBERS.\n");
+   printf("\nCHOOSE DIFFICULTY :\n");
+   printf("1. EASY     (1-50, 10 attempts)\n");
+   printf("2. MEDIUM   (1-100, 7 attempts)\n");
+   printf("3. HARD     (1-200, 6 attempts)\n");
+   printf("4. EXTREME  (1-500, 5 attempts)\n");
+   printf("enter your choice: ");
 }
