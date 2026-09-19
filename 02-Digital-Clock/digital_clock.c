be9ae31d8f5889b71c340095f290b79dcb7acea0
verma0909k <coding0909k@gmail.com>
2026-09-19 18:39:42 +0530

    signal handling ctrl c , input validation

--- 02-Digital-Clock/digital_clock.c
+++ 02-Digital-Clock/digital_clock.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #define RED "\033[1;31m"
 #define BLUE "\033[1;34m"
@@ -18,19 +19,29 @@
     #define SLEEP(x) sleep(x)
 #endif
 
+volatile sig_atomic_t running=1;
+
 void filltime(char string[],int format);
 void filldate(char string[]);
+void handler(int signum) {
+    running=0;
+}
 int main() {
+    signal(SIGINT,handler);
     printf(YELLOW"\nCHOOSE THE TIME FORMAT:\n");
     printf("1. 24 hour format\n");
     printf("2. 12 hour format(default)\n"RESET);
     printf("make a choice(1/2) :");
     int format;
     char time[50],date[100];
-    scanf("%d",&format);
+    while (scanf("%d",&format)!=1) {
+        printf(RED"ERROR: INVALID NUMBER\n"RESET);
+        printf("enter again : ");
+        while (getchar()!='\n'); 
+    }
     printf("\033[2J");
     printf("\033[?25l");
-    while(1) {
+    while(running) {
         printf("\033[H");
         filltime(time,format);
         filldate(date);
@@ -43,8 +54,8 @@ int main() {
         printf(RED"PRESS CTRL+C TO EXIT."RESET);
         fflush(stdout);
         SLEEP(1);
-        printf("\033[?25h");
     }
+    printf("\033[?25h");
     return 0;
 }
 void filldate(char string[100]) {
@@ -52,7 +63,7 @@ void filldate(char string[100]) {
     struct tm *timeinfo;
     time(&rawtime);
     timeinfo=localtime(&rawtime);
-    strftime(string,100,WHITE"%A %B %Y"RESET"]\n",timeinfo);
+    strftime(string,100,WHITE"%A %d %B %Y"RESET"]\n",timeinfo);
 }
 void filltime(char string[50],int format) {
     time_t rawtime;
